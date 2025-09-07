b012181ce73c07403ede6f61b0b21eb7c0e89685
BahopiyaDhaval <dhavalbahopiya92@gmail.com>
2025-09-07 22:31:56 +0530

    Add files via upload

---
+++ lab 4/lab 04 poat lab 02.py
@@ -0,0 +1,6 @@
+#Dhaval Bahopiya
+
+
+
+numbers = [10, 25, 3, 99, 56]
+print("Largest number is:", max(numbers))
---
+++ lab 4/lab 04 post lab 06.py
@@ -0,0 +1,9 @@
+#Dhaval Bahopiya
+
+numbers = [1, 2, 3, 4]
+result = 0
+
+for n in numbers:
+    result = result * 10 + n
+
+print(result)
